fa6292e40c97d8579009ca6a01dba9e3af53bc75
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2022-07-04 20:35:05 +0700

    laravel mix + componenst

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import "flowbite";
+import $ from "jquery";
+
+const feather = require("feather-icons");
+feather.icons.x;
+feather.icons.x.toSvg();
+
+feather.icons.x.toSvg({ class: "foo bar", "stroke-width": 1, color: "red" });
---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+let mix = require("laravel-mix");
+
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [
+        // add here
+        require("tailwindcss"),
+    ]
+);
